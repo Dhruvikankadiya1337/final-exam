20084fcc3affa0529eafc95dea89a771c4dcecd9
Dhruvikankadiya1337 <dhruvikankadiya@gmail.com>
2024-09-21 09:59:03 +0530

    Add files via upload

---
+++ q4.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+#include<conio.h>
+// wap to convet trmperature from degree celcius to fahrenfeit.
+
+void main()
+{
+	int celsius;
+	
+	printf("enter the degree of celcius");
+	scanf("%d",&celcius);
+	
+	(32°F - 32) × 5/9 = 0°C;
+	
+	degree=fahrenfeit*celcius;
+	
+	
+
+}
---
+++ q5.c
@@ -0,0 +1,22 @@
+#include<stdio.h>
+#include<conio.h>
+
+// wap
+void main()
+{
+	int n;
+	
+	printf("enter the n");
+	scanf("%d",&n);
+	
+	if(n%2==0)
+	{
+		printf("this number is even");
+	}
+	
+  else
+	{
+		prinf("this number is odd");
+	}
+
+}
